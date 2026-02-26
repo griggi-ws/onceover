0d10d9fde022c88afb5103f32071f35c26676745
griggi-ws <gavin.riggi@wellspring.com>
2026-02-26 18:17:33 -0500

    add: Deferred function testing to mock function checks

--- spec/fixtures/controlrepos/function_mocking/site/role/manifests/test_new_functions.pp
+++ spec/fixtures/controlrepos/function_mocking/site/role/manifests/test_new_functions.pp
@@ -2,11 +2,11 @@
 #
 #
 class role::test_new_functions {
-
   # This function should fail unless it is mocked
   profile::fail_puppet('Puppet language function still failed!')
 
   # So should this
   profile::fail_ruby('Ruby function failed!')
 
+  Deferred('profile::fail_ruby', ['Deferred Ruby function failed!'])
 }
