80217bc409075e30b721ba0494e2f7b5b86cef2d
griggi-ws <gavin.riggi@wellspring.com>
2026-02-26 18:18:07 -0500

    add: rspec tests that leverage the new helper for validation alongside existing cucumber suite

---
+++ spec/fixtures/controlrepos/caching/spec/classes/role__example_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Custom spec that uses Onceover::RSpec::Helper shared context
+# This tests the helper's integration with onceover-generated spec_helper
+
+require 'spec_helper'
+
+describe 'role::example' do
+  Onceover::RSpec::Helper.factsets.each do |factset|
+    context "on #{factset[:name]}" do
+      include_context 'onceover', factset
+
+      it { is_expected.to compile.with_all_deps }
+    end
+  end
+end
